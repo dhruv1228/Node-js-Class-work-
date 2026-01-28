b81adb1964c67a494cd44f62e210c0cf85d743a0
dhruv1228 <dhruvpachauri28@gmail.com>
2026-01-28 10:57:06 +0530

    Lecture 11

---
+++ Lecture 11/app.js
@@ -0,0 +1,18 @@
+//query params
+
+const express=require('express');
+const app=express();
+
+app.get('/product/:id',(req,res)=>{
+  console.log(req.params);
+  res.send("This is a product route");
+});
+
+app.get('/todo/:id',(req,res)=>{
+  console.log("todo",req.params.id);
+  res.send("This is a todo route");
+});
+
+app.listen(3000,()=>{
+  console.log("server is running");
+});
